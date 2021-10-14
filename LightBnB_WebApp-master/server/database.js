5eb80c346f991ade8f0a56f30c4765f810ac13e5
Russell Engebretson <russell.engebretson@gmail.com>
2021-10-14 20:17:55 +0000

    Add db functionality to getAllReservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -79,7 +79,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  // return getAllProperties(null, 2);
+  return pool
+  .query(`SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating
+  FROM reservations
+  JOIN property_reviews ON reservations.id = reservation_id 
+  JOIN properties ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY start_date
+  LIMIT $2;`, [guest_id, limit])
+  .then(res => {
+    return res.rows;
+  })
+  .catch(err => console.log(err));
 }
 exports.getAllReservations = getAllReservations;
 
