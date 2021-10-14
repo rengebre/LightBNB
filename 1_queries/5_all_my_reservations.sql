5cdd17120473bfb4fa775cc1ce71025333698080
Russell Engebretson <russell.engebretson@gmail.com>
2021-10-14 19:58:51 +0000

    Add login/out and sign up functionality to lightbnb

--- 1_queries/5_all_my_reservations.sql
+++ 1_queries/5_all_my_reservations.sql
@@ -1,4 +1,4 @@
-SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating
+SELECT reservations.id, properties.id, AVG(property_reviews.rating) AS average_rating
 FROM reservations
 JOIN property_reviews ON reservations.id = reservation_id 
 JOIN properties ON properties.id = property_reviews.property_id
